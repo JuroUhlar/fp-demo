6573647c5ddde8b2c7cf133d2fe4a3692dad2a07
Juraj Uhlar <juro.uhlar@gmail.com>
2023-03-27 10:51:01 +0100

    Stackblitz experiment

---
+++ src/pages/stackblitz.tsx
@@ -0,0 +1,12 @@
+export default function StackBlitzSandbox(): JSX.Element {
+  return (
+    <div>
+      <h1>StackBlitz Sandbox</h1>
+      <iframe
+        width="1400px"
+        height="800px"
+        src="https://stackblitz.com/github/fingerprintjs/fingerprintjs-pro-use-cases?embed=1&file=pages/api/web-scraping/flights.ts"
+      ></iframe>
+    </div>
+  );
+}
