84d0ca45acfb1bed25937c0f862a4b4d80f1dedf
Juraj Uhlar <juro.uhlar@gmail.com>
2023-06-16 15:41:58 +0100

    alternative constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,3 +5,9 @@ export const SERVER_API_KEY = process.env.SERVER_API_KEY ?? '2ittvzwZZThxLA36pKG
 export const CUSTOM_SUBDOMAIN = 'https://fp.jurajuhlar.eu';
 export const REGION = 'eu';
 export const REGION_SDK = Region.EU;
+
+// export const PUBLIC_API_KEY = process.env.NEXT_PUBLIC_PUBLIC_API_KEY ?? 'xvADip1wZkem5uyghtMj';
+// export const SERVER_API_KEY = process.env.SERVER_API_KEY ?? 'irOLwdzWwi9KwKCioLTo';
+// export const CUSTOM_SUBDOMAIN = 'https://fp.jurajuhlar.eu';
+// export const REGION = 'us';
+// export const REGION_SDK = Region.Global;
