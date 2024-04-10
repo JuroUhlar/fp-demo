dd37ab7c0b0e033753af036a6552859517f3832b
Juraj Uhlar <juro.uhlar@gmail.com>
2024-04-10 15:44:19 +0200

    update iframe

--- src/app/iframe/page.tsx
+++ src/app/iframe/page.tsx
@@ -4,9 +4,9 @@ const IframeTest = () => {
       <h1>Iframe test</h1>
       <p>This is a test that Google can render and index the iframe</p>
       <iframe
-        src="https://fingerprinthub.com/coupon-fraud/embed"
-        width="800"
-        height="900"
+        src="https://demo.fingerprint.com/coupon-fraud/embed"
+        width="1200"
+        height="1200"
         style={{ borderWidth: 0 }}
       ></iframe>
     </div>
