8fc6ee7df5f601d0da492359d8352b37bd105b45
Juraj Uhlar <juro.uhlar@gmail.com>
2024-04-27 15:24:22 +0100

    migration test

---
+++ src/app/cloudfront-migration-test-v1/page.tsx
@@ -0,0 +1,15 @@
+import { NpmPackageIdentificationDemo } from '../../components/JsAgentNpmDemo';
+
+export default function ExamplePage() {
+  return (
+    <NpmPackageIdentificationDemo
+      loadOptions={{
+        apiKey: '2UZgp3skqLzfJpFUGUrw',
+        endpoint: 'https://d2u1clkalj34vq.cloudfront.net/fpjsv1/result?region=eu',
+        scriptUrlPattern:
+          'https://d2u1clkalj34vq.cloudfront.net/fpjsv1/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
+      }}
+      name={'Cloudfront Migration test v1'}
+    />
+  );
+}
