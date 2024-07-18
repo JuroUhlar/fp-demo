5a25a87e094bd8c1ac4219672286c00a7857293c
Juraj Uhlar <juro.uhlar@gmail.com>
2024-07-18 20:22:54 +0100

    add integration

--- cloudfront-terraform-existing-distribution/main.tf
+++ cloudfront-terraform-existing-distribution/main.tf
@@ -1,18 +1,14 @@
-# Configure the AWS Provider
-provider "aws" {
-  region = "us-east-1" # CloudFront is a global service, but the API is in us-east-1
-}
-
+# Configure the AWS Provide
 # Create CloudFront distribution
-resource "aws_cloudfront_distribution" "jurajuhlar_distribution" {
+resource "aws_cloudfront_distribution" "main_website_distribution" {
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "CloudFront distribution for jurajuhlar.eu"
+  comment             = "CloudFront distribution for cloudfrontterraform.netlify.app"
   default_root_object = "index.html"
 
   origin {
-    domain_name = "jurajuhlar.eu"
-    origin_id   = "jurajuhlar-website"
+    domain_name = "cloudfrontterraform.netlify.app"
+    origin_id   = "netlify-website"
 
     custom_origin_config {
       http_port              = 80
@@ -25,7 +21,7 @@ resource "aws_cloudfront_distribution" "jurajuhlar_distribution" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "jurajuhlar-website"
+    target_origin_id = "netlify-website"
 
     forwarded_values {
       query_string = false
@@ -49,9 +45,45 @@ resource "aws_cloudfront_distribution" "jurajuhlar_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = true
   }
+
+
+  #region Fingerprint CloudFront Integration start
+  origin {
+    domain_name = "fpcdn.io"
+    origin_id   = local.fpcdn_origin_id
+    custom_origin_config {
+      origin_protocol_policy = "https-only"
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+    custom_header {
+      name  = "FPJS_SECRET_NAME"
+      value = module.fingerprint_cloudfront_integration.fpjs_secret_manager_arn
+    }
+  }
+
+  ordered_cache_behavior {
+    path_pattern = "fpjs/*"
+
+    allowed_methods          = ["GET", "HEAD", "OPTIONS", "PUT", "POST", "PATCH", "DELETE"]
+    cached_methods           = ["GET", "HEAD"]
+    cache_policy_id          = module.fingerprint_cloudfront_integration.fpjs_cache_policy_id
+    origin_request_policy_id = "216adef6-5c7f-47e4-b989-5492eafa07d3" # Default AllViewer policy
+    target_origin_id         = local.fpcdn_origin_id
+    viewer_protocol_policy   = "https-only"
+    compress                 = true
+
+    lambda_function_association {
+      event_type   = "origin-request"
+      lambda_arn   = module.fingerprint_cloudfront_integration.fpjs_proxy_lambda_arn
+      include_body = true
+    }
+  }
+  #endregion
 }
 
 # Output the CloudFront domain name
 output "cloudfront_domain_name" {
-  value = aws_cloudfront_distribution.jurajuhlar_distribution.domain_name
+  value = aws_cloudfront_distribution.main_website_distribution.domain_name
 }
---
+++ cloudfront-terraform-existing-distribution/variables.tf
@@ -0,0 +1,4 @@
+variable "proxy_secret" {
+  description = "The proxy secret for the Fingerprint proxy integration"
+  type        = string
+}
