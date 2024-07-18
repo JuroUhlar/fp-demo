023612d950c20eb691ef2315d048e86e3ba512e7
Juraj Uhlar <juro.uhlar@gmail.com>
2024-07-18 21:15:27 +0100

    deploy example

---
+++ src/app/cloudfront-terraform-existing-distribution/page.tsx
@@ -0,0 +1,23 @@
+import { NpmPackageIdentificationDemo } from '../../components/JsAgentNpmDemo';
+import { SUBS } from '../../constants';
+
+// const host = 'd3cu7lsl52iiaf.cloudfront.net';
+const host = 'cloudfront-via-terraform-existing-distro.juraj.click';
+
+export default function ExamplePage() {
+  return (
+    <>
+      <NpmPackageIdentificationDemo
+        loadOptions={{
+          apiKey: SUBS.identificationOnly.loadOptions.apiKey,
+          endpoint: `https://${host}/fpjs/result?region=us`,
+          scriptUrlPattern: `https://${host}/fpjs/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>`,
+        }}
+        name={'Cloudfront Terraform Existing Distribution'}
+      />
+      <h3>
+        Can also visit the main site at <a href={`https://${host}`}>{host}</a>
+      </h3>
+    </>
+  );
+}
