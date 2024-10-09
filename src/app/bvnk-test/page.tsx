687c2295885012744c26d10969311c3ce11dc539
Juraj Uhlar <juro.uhlar@gmail.com>
2024-10-09 16:23:54 +0200

    bvnk test

---
+++ src/app/bvnk-test/page.tsx
@@ -0,0 +1,22 @@
+import { NpmPackageIdentificationDemo } from '../../components/JsAgentNpmDemo';
+import { SUBS } from '../../constants';
+
+// const host = 'd3cu7lsl52iiaf.cloudfront.net';
+// const host = 'cloudfront-via-terraform.juraj.click';
+// const host = 'd18gupksmr7h1t.cloudfront.net';
+const host = 'metrics.staging.bvnk.com';
+
+export default function ExamplePage() {
+  return (
+    <NpmPackageIdentificationDemo
+      loadOptions={{
+        apiKey: SUBS.main.loadOptions.apiKey,
+        endpoint: `https://${host}/x8vwd7gt3387kovmx4sf?region=eu`,
+        scriptUrlPattern: `https://${host}/6ger4isjyjgno3bjt3ky/?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>`,
+      }}
+      name={'Cloudfront Terraform New Distribution'}
+      serverApiRegion={SUBS.main.serverApiRegion}
+      serverApiKey={SUBS.main.serverApiKey}
+    />
+  );
+}
