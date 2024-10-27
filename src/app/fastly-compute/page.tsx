7e62d89e582de25a2968824b8ae65a72993751eb
Juraj Uhlar <juro.uhlar@gmail.com>
2024-10-27 12:46:50 +0000

    deploy new fastly compute

--- src/app/fastly-compute/page.tsx
+++ src/app/fastly-compute/page.tsx
@@ -1,46 +1,7 @@
-// 'use client';
-// // import * as FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
-// import { useEffect, useState } from 'react';
-// import FingerprintJS, { GetResult } from '@fingerprintjs/fingerprintjs-pro';
-
-// export default function ClientPage() {
-//   useEffect(() => {
-//     // Initialize an agent at application startup.
-//     const fpPromise = FingerprintJS.load({
-//       apiKey: 'xvADip1wZkem5uyghtMj',
-//       scriptUrlPattern: [
-//         'https://fastly4.jurajuhlar.eu/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
-//         FingerprintJS.defaultScriptUrlPattern, // Fallback to default CDN in case of error
-//       ],
-//       endpoint: [
-//         'https://fastly4.jurajuhlar.eu/result',
-//         FingerprintJS.defaultEndpoint, // Fallback to default endpoint in case of error
-//       ],
-//     });
-
-//     // Analyze the visitor when necessary.
-//     fpPromise
-//       .then((fp) => fp.get({ extendedResult: true }))
-//       .then((result) => {
-//         console.log(result.requestId, result.visitorId);
-//         setFingerprintData(result);
-//       });
-//   }, []);
-
-//   const [fingerprintData, setFingerprintData] = useState<GetResult | null>(null);
-
-//   return (
-//     <>
-//       <main className="m-10">
-//         <h1>Fastly integration</h1>
-//         <pre>{JSON.stringify(fingerprintData, null, 2)}</pre>
-//       </main>
-//     </>
-//   );
-// }
-
 import { NpmPackageIdentificationDemo } from '../../components/JsAgentNpmDemo';
 import { REGION_SDK, SERVER_API_KEY, SUBS } from '../../constants';
+
+const integrationUrl = 'https://fastly-compute.jurajuhlar.site';
 
 export default function ExamplePage() {
   return (
@@ -48,9 +9,8 @@ export default function ExamplePage() {
       loadOptions={{
         apiKey: SUBS.openResponse.loadOptions.apiKey,
         region: SUBS.openResponse.loadOptions.region,
-        endpoint: 'https://fastly4.jurajuhlar.eu/result',
-        scriptUrlPattern:
-          'https://fastly4.jurajuhlar.eu/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>',
+        endpoint: `${integrationUrl}/result`,
+        scriptUrlPattern: `${integrationUrl}/agent?apiKey=<apiKey>&version=<version>&loaderVersion=<loaderVersion>`,
       }}
       name={'Fastly Compute Integration. Open Response'}
       serverApiKey={SUBS.openResponse.serverApiKey}
