ac886c13dff67740489721af50448fc5bd692eb1
Juraj Uhlar <juro.uhlar@gmail.com>
2026-04-02 14:04:48 +0100

    Add Cloudflare manual setup v4 page: create a new page component integrating NpmPackageIdentificationDemoV4 with updated start options and endpoint configuration for enhanced functionality.

---
+++ src/app/cloudflare-manual-setup-v4/page.tsx
@@ -0,0 +1,25 @@
+import { NpmPackageIdentificationDemoV4 } from '../../components/JsAgentNpmDemo_v4';
+import { SUBS } from '../../constants';
+
+// const host = 'd3cu7lsl52iiaf.cloudfront.net';
+// const host = 'fingerprint-proxy-integration.juraj-uhlar.workers.dev';
+const host = 'manual-cf.jurajuhlar.site';
+
+export default function CloudflareManualSetup() {
+  const sub = SUBS.main;
+  const { apiKey, region } = sub.loadOptions;
+  const { serverApiKey } = sub;
+
+  return (
+    <NpmPackageIdentificationDemoV4
+      startOptions={{
+        apiKey,
+        region,
+        endpoints: `https://${host}?region=eu`,
+      }}
+      name={'Cloudflare manual setup Agent v4, integration path depth = 0'}
+      serverApiKey={serverApiKey}
+      serverApiRegion={sub.serverApiRegion}
+    />
+  );
+}
